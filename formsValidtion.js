99acd338f64ebe2fa6e982d782f7a61b3f5f79ac
pavel359 <pavel.malchuganov@mail.ru>
2020-08-09 03:14:59 +0600

    Add files via upload

---
+++ formsValidtion.js
@@ -0,0 +1,10 @@
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("I am expecting an e-mail address!");
+    } else {
+        email.setCustomValidity("");
+    }
+});
+
