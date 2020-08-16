1b944ca120ac20fc823cfe1b965cd535293c23d4
pavel359 <pavel.malchuganov@mail.ru>
2020-08-16 22:14:14 +0600

    Add files via upload

--- formsValidtion.js
+++ formsValidtion.js
@@ -1,3 +1,34 @@
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+        textbox.addEventListener(event, function() {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    });
+}
 
+setInputFilter(document.getElementById("cardnumber"), function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+});
+
+setInputFilter(document.getElementById("pin"), function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+});
+
+setInputFilter(document.getElementById("cvv"), function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+});
+
+setInputFilter(document.getElementById("phone"), function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+});
 
 
